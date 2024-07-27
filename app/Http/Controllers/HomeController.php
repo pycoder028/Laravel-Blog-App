7d5775ce52d8e8b274ebf7ca56d3609b87c10b8b
Forhad-Hossain <pycoder028@gmail.com>
2024-07-28 02:03:41 +0600

    Show Post Data From Database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,11 +11,12 @@ use App\Models\User;
 class HomeController extends Controller
 {
     public function index(){
+        $post = Post::all();
         if(Auth::id()){
             $usertype = Auth()->user()->usertype;
 
             if($usertype == 'user'){
-                return view('home.master');
+                return view('home.master',compact('post'));
             }else if($usertype == 'admin'){
                 return view('admin.adminhome');
             }else{
@@ -25,7 +27,9 @@ class HomeController extends Controller
 
     public function homepage(){
 
-        return view('home.master');
+        $post = Post::all();
+
+        return view('home.master',compact('post'));
     }   // end method here
     
 
