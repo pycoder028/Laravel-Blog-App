ccd5a34e6045aa8fe5489e7cb276cfb72d5f18a7
Forhad-Hossain <pycoder028@gmail.com>
2024-07-27 18:04:56 +0600

    Integrate Admin Template

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -1,17 +1,20 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html>
+  <head> 
+    @include('admin.partials.css')
+  </head>
+  <body>
+    @include('admin.partials.header')
+    
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+        @include('admin.partials.sidebar')
+      <!-- Sidebar Navigation end-->
+        @include('admin.partials.body')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in as Admin!") }}
-                </div>
-            </div>
-        </div>
+        @include('admin.partials.footer')
     </div>
-</x-app-layout>
+    <!-- JavaScript files-->
+    @include('admin.partials.scripts')
+  </body>
+</html>
